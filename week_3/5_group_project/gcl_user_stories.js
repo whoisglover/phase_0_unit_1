1e987e83154c0da43bf890791c3f18d1d1c94d01
whoisglover <dannyglover@outlook.com>
2014-04-17 23:04:45 -0700

    group project

---
+++ week_3/5_group_project/gcl_user_stories.js
@@ -0,0 +1,30 @@
+//Test 1:
+// I want to create a function called sum.
+
+//Test 2:
+// I want to 
+
+//Test 3:
+// I want to 
+
+//Test 4:
+// I want to 
+
+//Test 5:
+// I want to 
+
+//Test 6:
+// I want to 
+
+//Test 7:
+// I want to 
+
+//Test 8:
+// I want to 
+
+//Test 9:
+// I want to 
+
+
+
+
---
+++ week_3/5_group_project/my_solution.js
@@ -0,0 +1,30 @@
+//Test 1:
+// I want to create a mini program called sum.
+
+//Test 2:
+// I have list of numbers.  The list contains an odd amount of numbers. I want the sum program to add all of these up and tell me what the sum is.
+
+//Test 3:
+// I have list of numbers.  The list contains an even amount of numbers. I want the sum program to add all of these up and tell me what the sum is.
+
+//Test 4:
+// I want to create a new mini program called mean.
+
+//Test 5:
+// I have the same list of numbers that is odd from case 2 and I want mean to give me the average of these numbers.
+
+//Test 6:
+// I have the same list of numbers that is even from case 3 and I want mean to give me the average of these numbers.
+
+//Test 7:
+// I want to create a new mini program called median.
+
+//Test 8:
+// I want the mini program median to give me the median value of a list of numbers with an odd amount.
+
+//Test 9:
+// I want the mini program median to give me the median value of a list of numbers with an even amount.
+
+
+
+
