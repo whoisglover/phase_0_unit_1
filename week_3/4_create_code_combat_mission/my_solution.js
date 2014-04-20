d2132c5336086559e580cca5882510257bdba3a7
whoisglover <dannyglover@outlook.com>
2014-04-20 12:30:39 -0700

    finish challenge 4

--- week_3/4_create_code_combat_mission/my_solution.js
+++ week_3/4_create_code_combat_mission/my_solution.js
@@ -48,5 +48,18 @@ Hulk.moveRight();
 console.log(Hulk.positionX);
 
 
+//Reflection 
+/* 
+This challenge was completed during a pairing session with Matt Kuzio. In all aspects it was 
+a really successful pairing.  We had a good intro and discussed our opinions on the differences
+between driving and navigating.  We then moved onto the pseduo code and discussed what we thought
+the goal of the challenge was.  We started with Matt navigating since he said he hadn't been able to
+in his previous pairings. The code turned out pretty good and my instructors advice from GPS 1.2
+was to test early and often which helped Matt and I here.  After about 20 minutes we switched roles
+and were able to finish the main code for the character object.  Then we did some basic displaying
+to make sure that our built in functions for movement were working correctly.  We wrapped it up in just 
+about an hour and I would say I have very little suggestions for how the pairing couldve gone better.
+We also spent some time discussing the terminal on mac and I was able to share some of my experience
+navigating the command line with Matt which I hope will help him in the future.
 
-
+*/
