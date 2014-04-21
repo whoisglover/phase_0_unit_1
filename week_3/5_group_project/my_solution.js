219ac34bdf4805951d5f319956c93d2cb995506e
Danny Glover <dannyglover@outlook.com>
2014-04-20 21:54:26 -0700

    Update my_solution.js

--- week_3/5_group_project/my_solution.js
+++ week_3/5_group_project/my_solution.js
@@ -26,5 +26,67 @@
 // I want the mini program median to give me the median value of a list of numbers with an even amount.
 
 
+//I am Person 1 in the group after our group worked through the code this is our perfectly passing solution:
+
+function sum (arr) {
+  var total = 0;
+  for(var i=0; i < arr.length; i++) {
+    total+= arr[i];
+  }
+  return total;
+}
+
+
+function mean (arr) {
+  var arraySum= sum(arr);
+  return (arraySum/arr.length);
+}
+
+function median (arr) {
+  var sortedArray= arr.sort();
+  if (sortedArray.length%2===0) {
+    return (((sortedArray[sortedArray.length/2])+(sortedArray[(sortedArray.length-2)/2]))/2)
+  }
+  else  {
+    return sortedArray[(sortedArray.length-1)/2];
+}
+}
+
+
+// __________________________________________
+// Tests:  Do not alter code below this line.
+
+
+oddLengthArray  = [1, 2, 3, 4, 5, 5, 7]
+evenLengthArray = [4, 4, 5, 5, 6, 6, 6, 7]
+
+
+function assert(test, message, test_number) {
+  if (!test) {
+    console.log(test_number + "false");
+    throw "ERROR: " + message;
+  }
+  console.log(test_number + "true");
+  return true;
+}
+
+// tests for sum
+assert(
+  (sum instanceof Function),
+  "sum should be a Function.",
+  "1. "
+)
+
+assert(
+  sum(oddLengthArray) === 27,
+  "sum should return the sum of all elements in an array with an odd length.",
+  "2. "
+)
+
+"group.js" 98L, 1959C
+
+
+
+
 
 
